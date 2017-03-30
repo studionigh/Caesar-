0240ac331f4ee1c4e00c60e4afb06a182ab14ecf
studionigh <studionigh@gmail.com>
2017-03-30 15:10:00 -0500

    good try, but no cigar

--- Caesar/Program.cs
+++ Caesar/Program.cs
@@ -10,6 +10,40 @@ namespace Caesar
     {
         static void Main(string[] args)
         {
+            
+            {
+                //Get users input for key
+                Console.Write("Enter your encription key (number)");
+                string userKey = Console.ReadLine();
+                int kripKey = int.Parse(userKey);
+
+                //Get users input for text to encript
+                Console.WriteLine("Enter text to be encripted");
+                string userText = Console.ReadLine();
+                var Characters = userText.ToCharArray();
+
+                int key;
+
+                for (int i = 0; i < Characters.Length; i++)
+                {
+                    char letter = Characters[i];
+                    letter = (char)(letter + kripKey);
+                    if (letter > 'z')
+                    {
+                        letter = (char)(letter - 26);
+                    }
+                    else if (letter < 'a')
+                    {
+                        letter = (char)(letter + 26);
+                    }
+                    Console.WriteLine(letter  + kripKey);
+                }
+               // Console.WriteLine(Characters);
+                Console.ReadKey();
+            }
+            
         }
+
+       
     }
 }
